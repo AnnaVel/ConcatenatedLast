01f904841aac8a15bed355eb3d727343f944ded4
PROGRESS\velcheva <velcheva@progress.com>
2024-10-02 11:24:20 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        IEnumerable<int> results = new List<int>() { 1, 2, 3 }.Concat(new List<int>()).Concat(new List<int>());
+
+        bool any = results.Any(); // true
+        int count = results.Count(); // 3
+        int last = results.Last(); // 'Sequence contains no elements.'
+    }
+}
